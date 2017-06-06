db0a7b9d8210498abc9c9e0f4d8e05ab3a5091fe
André Nunes <andrenunes2005@gmail.com>
2017-06-05 22:49:33 -0300

    initial commit with react and nodejs app

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2> Nossa História </h2>
+        <p> Lorem Ipsum dolor sit amet... </p>
+        <h2> Missão e visão </h2>
+        <p> Lorem Ipsum dolor sit amet... </p>
+        <h2> Imprensa </h2>
+        <p> Lorem Ipsum dolor sit amet... </p>
+    </div>
+)
